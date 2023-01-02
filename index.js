0cbf1748199ddd3cc7b3d6ffbac0e50724b8823e
Ramiz Ali <ramiz.apk7@gmail.com>
2023-01-02 04:49:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(style="*"){
+    const innerFunction = function (adjective="special") {
+        // 3
+        return `You are ${style}${adjective}${style}!`; 
+      };
+      return innerFunction;
+}
